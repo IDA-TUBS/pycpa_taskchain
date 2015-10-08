1b285d629b7a5ae4dd7ed841f3577b4e0deacd92
Johannes Schlatow <schlatow@ida.ing.tu-bs.de>
2015-10-08 19:16:40 +0200

    adapt eval script

--- eval.sh
+++ eval.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-python examples/plot_scatter_path.py path_report.csv --relative --original lat1a --improved lat1b
-python examples/plot_scatter_path.py path_report.csv --relative --original lat2a --improved lat2b
-python examples/plot_scatter_path.py path_report.csv --relative --original lat3a --improved lat3b
-python examples/plot_scatter_path.py path_report.csv --relative --original lat4a --improved lat4b
-python examples/plot_scatter_path.py path_report.csv --relative --original lat5a --improved lat5b
+python examples/plot_scatter_path.py path_report.csv --relative --original lat --improved lat_sync
+python examples/plot_scatter_path.py path_report.csv --relative --original lat --improved lat_async
+python examples/plot_scatter_path.py path_report.csv --relative --original lat_sync --improved lat_syncref
