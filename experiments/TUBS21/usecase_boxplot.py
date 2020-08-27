c924bbbd97988674240654e9542bc37fb1f8ba6b
Johannes Schlatow <johannes@schlatow.name>
2020-08-27 22:45:39 +0200

    experiments: update plot script

--- experiments/TUBS21/usecase_boxplot.py
+++ experiments/TUBS21/usecase_boxplot.py
@@ -15,6 +15,7 @@ Description
 
 import argparse
 from eval import usecase
+from pandas.api.types import CategoricalDtype
 
 import pandas as pd
 import seaborn as sns
@@ -25,6 +26,12 @@ parser.add_argument('folder', type=str,
         help="Folder containing the subfolders with different analysis results in separate results.csv files.")
 parser.add_argument('--xlabel', type=str)
 parser.add_argument('--ylabel', type=str)
+parser.add_argument('--width', type=float, default=7)
+parser.add_argument('--height', type=float, default=4)
+parser.add_argument('--boxplot', action='store_true')
+parser.add_argument('--order', type=str, nargs='*', default=list())
+parser.add_argument('--pathorder', type=str, nargs='*', default=list())
+parser.add_argument('--pathnames', type=str, nargs='*', default=list())
 parser.add_argument('--output', default=None, required=False,
                         help='save plot to given file')
 
@@ -35,23 +42,45 @@ if __name__ == "__main__":
     data.process()
     df = data.unpivot()
 
+    if args.order:
+        df['variable'] = df['variable'].astype(CategoricalDtype(categories=args.order, ordered=True))
+
+    if args.pathorder:
+        df['Path'] = df['Path'].astype(CategoricalDtype(categories=args.pathorder, ordered=True))
+
+    if args.pathnames:
+        assert args.pathorder and len(args.pathorder) == len(args.pathnames)
+        df['Path'] = df['Path'].cat.rename_categories(args.pathnames)
+
     sns.set(style="darkgrid", palette="dark")
-    g = sns.catplot(x='variable', y='value', data=df, kind='box', color='lightgray',
-                    width=0.3,
-                    showfliers=False,
-                    linewidth=2)
-    sns.swarmplot(x='variable', y='value', hue='Path', data=df, ax=g.ax,
-                  size=7,
+
+    fig = plt.figure(figsize=(args.width, args.height))
+    ax = fig.add_subplot()
+
+    if args.boxplot:
+        sns.catplot(x='variable', y='value', data=df, kind='box', color='lightgray',
+                        width=0.3,
+                        showfliers=False,
+                        ax=ax,
+                        linewidth=2)
+
+    sns.stripplot(x='variable', y='value', hue='Path', data=df, ax=ax,
+                  size=2,
                   dodge=True)
+
     sns.despine(left=True)
 
     if args.xlabel:
-        g.ax.set_xlabel(args.xlabel)
+        ax.set_xlabel(args.xlabel)
 
     if args.ylabel:
-        g.ax.set_xlabel(args.ylabel)
+        ax.set_ylabel(args.ylabel)
+
+    if args.order and len(args.order) == 1:
+        a, b = plt.xlim()
+        plt.xlim(2*a, 2*b)
 
     if args.output:
-        plt.savefig(output, bbox_inches='tight')
+        plt.savefig(args.output, bbox_inches='tight')
     else:
         plt.show()
